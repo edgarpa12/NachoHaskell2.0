6d28f1015c99bdca1dd03338ad914d6fdb76855f
Edgar <edgdav00@gmail.com>
2020-08-15 19:11:48 -0600

    Avance Tratamiento Palabras

--- main.hs
+++ main.hs
@@ -1,14 +1,33 @@
 import System.IO
+import Data.Char
+import Data.List
 
-
-
+main :: IO ()
 main = do
-    putStrLn "Digite un nombre Plebe:"
-    texto <- getLine
-    putStrLn "Digite otro nombre Plebe:"
+    --putStrLn "Digite el nombre del archivo con los textos:"
+    --titulos <- getLine
+    putStrLn "Digite el nombre con el archivo con palabras no significativas:"
+    palabrasNS <- getLine
+    putStrLn "Digite el nombre con el archivo de salida:"
     output <- getLine
-    handle <- openFile texto ReadMode
-    contents <- hGetContents handle
+    handleTitulos <- openFile "titulos.txt" ReadMode
+    contents <- hGetContents handleTitulos
+    let todoTasks = lines contents     
+        --numberedTasks = zipWith (\n line -> "como") [0..] todoTasks
+        separarPalabras = map (split ' ') todoTasks
+        --editarPalabra = zipWith (\n line -> line ++ "pollo\n") [0..] (map unlines separarPalabras)
+        --editarPalabra = filter (=="airplane") (map unlines separarPalabras)
+        --editarPalabra = (map (=="airplane") separarPalabras)
+
+    putStrLn ""
+    print (map (filter (=="teapot")) separarPalabras)
+    print separarPalabras
+    --putStrLn $ unlines editarPalabra
+    --putStrLn $ unlines (map unlines separarPalabras)
     writeFile output contents
-    putStrLn contents
-    hClose handle
+    hClose handleTitulos
+
+
+split :: Eq a => a -> [a] -> [[a]]
+split d [] = []
+split d s = x : split d (drop 1 y) where (x,y) = span (/= d) s
